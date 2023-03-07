053b2ce9beff475f21b3fa2c353244779df0c74e
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-03-07 21:05:36 +0100

    Initialized canvas declared Height & Width

--- js/script.js
+++ js/script.js
@@ -14,6 +14,14 @@ img.src = '../images/bg.png';
   const obstacleTop = new Image();
   obstacleTop.src = '../images/obstacle_top.png';
 
+  const obstacleArray = [];
+ let countedScore = 0;
+//Declares Canvas and background Image
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d');
+canvas.height = 600;
+canvas.width = 800;
+
   }
 
 };
