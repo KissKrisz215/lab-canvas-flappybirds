3922bef46c292618a3ea25a08474e9e05785689f
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-03-07 21:04:57 +0100

    Initialized Image Files for Character, Background & Obstacles

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ window.onload = function() {
   };
 
   function startGame() {
+//Load new Images
+const img = new Image();
+img.src = '../images/bg.png';
+  const flappyBirdImg = new Image();
+  flappyBirdImg.src = '../images/flappy.png'
+  const ostacleBottom = new Image();
+  ostacleBottom.src = '../images/obstacle_bottom.png';
+  const obstacleTop = new Image();
+  obstacleTop.src = '../images/obstacle_top.png';
 
   }
 
