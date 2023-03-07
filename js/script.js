4ad269655b4d031d25562cba6fba9c98e7dc0749
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-03-07 21:24:51 +0100

    Added Function to rotate Character

--- js/script.js
+++ js/script.js
@@ -263,4 +263,19 @@ function drawScore(){
   ctx.font = '26px serif';
   ctx.fillText(`Score: ${flappyBird.score}`,650,35);
 }
+
+function drawImageRot(img,x,y,width,height,deg){
+  // Store the current context state (i.e. rotation, translation etc..)
+  ctx.save()
+  //Convert degrees to radian 
+  var rad = deg * Math.PI / 180;
+  //Set the origin to the center of the image
+  ctx.translate(x + width / 2, y + height / 2);
+  //Rotate the canvas around the origin
+  ctx.rotate(rad);
+  //draw the image    
+  ctx.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
+  // Restore canvas state as saved from above
+  ctx.restore();
+}
 };
