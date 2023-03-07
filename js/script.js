8bafefc42b54e9432b4fa183a82c7506968515c4
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-03-07 21:06:28 +0100

    Added Character Class and properties

--- js/script.js
+++ js/script.js
@@ -22,6 +22,22 @@ const ctx = canvas.getContext('2d');
 canvas.height = 600;
 canvas.width = 800;
 
-  }
 
+class character {
+  constructor(width,height){
+    this.width = width,
+    this.height = height,
+    this.x = 350,
+    this.y = 250,
+    this.speedX = 0,
+    this.speedY = 0,
+    this.gravity = 0,
+    this.gravitySpeed = 0,
+    this.img = flappyBirdImg,
+    this.score = 0,
+    this.moves = false,
+    this.fallingPoint = 0;
+    this.bestScore = 0
+  }
+  }
 };
